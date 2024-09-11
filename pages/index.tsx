f6df0818b095ec1c391a0cc7c19ca8d8c8c54599
fullkevinriveradev <hikevindev@gmail.com>
2024-09-11 14:32:56 +0200

    feat: añadir wsp button

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,8 @@ import Experience from "@/components/Experience";
 import { Projects } from "@/components/Projects";
 import { Profile } from "@/components/Profile";
 import Contact from "@/components/Contact";
-import { ToastContainer, toast } from "react-toastify";
+import { FloatingWhatsApp } from "react-floating-whatsapp";
+import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 export default function Home() {
@@ -17,6 +18,12 @@ export default function Home() {
       <Profile />
       <Contact />
       <ToastContainer />
+      <FloatingWhatsApp
+        phoneNumber="34654470578"
+        accountName="Kevin Rivera"
+        chatMessage="Hola! Me encataría conocer tu proyecto, escribeme para saber más."
+        darkMode={true}
+      />
     </section>
   );
 }
